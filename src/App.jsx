dd7d55db2689cffaea9fc5a2138af480da1104c1
developermohammedfazil <mjuman449@gmail.com>
2025-07-15 10:51:50 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React,{ useState } from 'react'
-import Navbar from './component/Navbar/Navbar'
-import {Route,Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Video from './pages/Video/Video'
+import React, { useState } from 'react';
+import Navbar from './component/Navbar/Navbar';
+import Sidebar from './component/Sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Video from './pages/Video/Video';
+
 const App = () => {
+  const [sidebar, setSidebar] = useState(true);
+  const [category, setCategory] = useState(0);
 
-  const[sidebar,setSidebar] = useState(true);
   return (
-    <div>
-  <Navbar setSidebar={setSidebar}/>
-  <Routes>
-    <Route path='/' element={<Home sidebar={sidebar}/>} />
-    <Route path='/video/:categoryId/:videoId' element={<Video/>} />
-  </Routes>
+    <div className="app-layout">
+      <Navbar setSidebar={setSidebar} />
+      <Sidebar sidebar={sidebar} category={category} setcategory={setCategory} />
+      <div className={`main-content ${sidebar ? 'sidebar-open' : 'sidebar-closed'}`}>
+        <Routes>
+          <Route path="/" element={<Home sidebar={sidebar} />} />
+          <Route path="/video/:categoryId/:videoId" element={<Video />} />
+        </Routes>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
